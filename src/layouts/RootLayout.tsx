7852f97d4b40bfaa2c06f0ca2affb7b6eb3dd0d9
Mahmood Hassan Rameem <rameem2019@gmail.com>
2025-11-24 18:55:22 +0600

    feat: add RootLayout component with Outlet and Footer

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import Navbar from "../components/shared/Navbar";
+// import { useContext } from "react";
+// import Navbar from "../components/shared/Navbar";
 import { Outlet } from "react-router";
-import { MenuToggleContext } from "../context/MenuToggleProvider";
-import { HiBars3 } from "react-icons/hi2";
+// import { MenuToggleContext } from "../context/MenuToggleProvider";
+// import { HiBars3 } from "react-icons/hi2";
 import Footer from "../components/shared/Footer";
 
 const RootLayout = () => {
