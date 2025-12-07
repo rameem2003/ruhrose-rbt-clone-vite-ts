e7b8d670218cc3fdf648d2d45ca122e1eb474c30
rameem2003 <rameem2019@gmail.com>
2025-12-07 15:17:40 +0600

    update

--- src/components/screens/Home/Varity.tsx
+++ src/components/screens/Home/Varity.tsx
@@ -15,26 +15,6 @@ import sustainiability2 from "../../../assets/sustainiability2.jpg";
 import production from "../../../assets/production.jpg";
 import about from "../../../assets/about1.jpg";
 
-const imagess = [
-  "https://images.unsplash.com/photo-1522943854798-7ea1c5464eba?q=80&w=3274&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1498435999018-6803de1f1c1f?q=80&w=3273&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1541695226403-a09aa08e5135?q=80&w=3276&auto=format&fit=crop",
-
-  "https://images.unsplash.com/photo-1609951651467-713256d1a3be?q=80&w=3328&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?q=80&w=3181&auto=format&fit=crop",
-
-  "https://images.unsplash.com/34/BA1yLjNnQCI1yisIZGEi_2013-07-16_1922_IMG_9873.jpg?q=80&w=3271&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1581472723648-909f4851d4ae?q=80&w=3270&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1635321856129-af8531379f4b?q=80&w=3000&auto=format&fit=crop",
-
-  "https://images.unsplash.com/photo-1586348943529-beaae6c28db9?q=80&w=3230&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1579888286284-692fa4bdc42b?q=80&w=3280&auto=format&fit=crop",
-
-  "https://images.unsplash.com/photo-1529369623266-f5264b696110?q=80&w=3174&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1583513702439-2e611c58e93d?q=80&w=3269&auto=format&fit=crop",
-  "https://images.unsplash.com/photo-1618220179428-22790b461013?q=80&w=3327&auto=format&fit=crop",
-];
-
 const images = [
   small1,
   small2,
